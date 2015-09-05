f88a317afbff1439665419f6e82cfb6b3c9a86f9
buchenglei <mybc724@163.com>
2015-09-05 10:38:03 +0800

    完善开放定址法实现的hash表

--- hash/hashquad.c
+++ hash/hashquad.c
@@ -1,3 +1,7 @@
+/*
+ * 开放定址法解决散列表中的冲突
+ *
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include "hashquad.h"
@@ -14,7 +18,10 @@ Index Hash(const char *Key, int TableSize) {
 }
 
 void main() {
-    
+    HashTable H = InitTable(17);
+    Insert("aaa", H);
+    int pos = Find("aaa", H);
+    printf("Finded pos is %d, value is %s\n", pos, H->TheCells[pos].Element);
 }
 
 HashTable InitTable(int TableSize) {
@@ -64,7 +71,9 @@ Position Find(ElementType Key, HashTable H) {
     CurrentPos = Hash(Key, H->TableSize);
     while(H->TheCells[CurrentPos].Info != Empty &&
             H->TheCells[CurrentPos].Element != Key) {
-        CurrentPos += 2 * ++CollisionNum - 1;
+        // 平方探测
+        // 寻找下一个空闲的存储单元
+        CurrentPos += 2 * ++CollisionNum - 1;   // 解决冲突的办法
         if(CurrentPos >= H->TableSize)
             CurrentPos -= H->TableSize;
     }
@@ -74,6 +83,8 @@ Position Find(ElementType Key, HashTable H) {
 void Insert(ElementType Key, HashTable H) {
     Position Pos;
     
+    // 查找该关键字是否存在
+    // 不存在就返回一个空闲地址
     Pos = Find(Key, H);
     if(H->TheCells[Pos].Info != Legitimate) {
         // ok to insert here
